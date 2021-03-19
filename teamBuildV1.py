7361cc0df6bc46f655d3c01adda1243102a2056e
vuxain <vukasinradic96@hotmail.com>
2021-03-19 19:30:44 +0100

    Update teamBuildV1.py

--- teamBuildV1.py
+++ teamBuildV1.py
@@ -18,6 +18,8 @@ from fpl import FPL
 
 import queue
 
+import copy
+
 
 async def main():
     async with aiohttp.ClientSession() as session:
@@ -25,13 +27,13 @@ async def main():
         
         class Node:
         
-            def __init__(self, level, weight, value, inserted, parent, position, positionState):
+            def __init__(self, level, weight, value,  parent, inserted ,position, positionState):
                 self.level = level
                 self.weight = weight
                 self.value = value
                 self.bound = 0.0
-                self.parent = parent
                 self.inserted = inserted
+                self.parent = parent
                 self.position = position
                 self.positionState = positionState
             
@@ -61,7 +63,7 @@ async def main():
         def bnb(knapsackWeight, items):
             Q = queue.Queue()
             # pogodno je inicijalizovati cvorom na nivou -1
-            u = Node(-1, 0, 0, 0, None, None, [0,2,5,5,3])
+            u = Node(-1, 0, 0, None, 0, None, [0,2,5,5,3])
             Q.put(u)
             maxValue = 0
             finalNode = None
@@ -69,39 +71,46 @@ async def main():
             while not Q.empty():
                 # u je trenutni cvor koji se razmatra
                 u = Q.get()
+                
                 if (u.level == len(items) - 1): 
                     continue
                 # v ovde predstavlja cvor za nivo nize pod pretpostavkom da je uzet cvor v
-                v = Node(u.level + 1, u.weight + items[u.level + 1].now_cost/10, u.value + items[u.level + 1].total_points, 1, u, items[u.level + 1].element_type,decrementList( u.positionState,items[u.level + 1].element_type))
+                uCopyPositionState =  copy.deepcopy(u.positionState) 
+                v = Node(u.level + 1, u.weight + items[u.level + 1].now_cost/10, u.value + items[u.level + 1].total_points, u, 1, items[u.level + 1].element_type,decrementList(uCopyPositionState,items[u.level + 1].element_type))
                 # provera da li se povecava maksimalna vrednost uzimajuci v u obzir 
                 
-                if  v.positionState[v.position] >0:
-                    if v.weight <= knapsackWeight and v.value > maxValue :
-                            maxValue = v.value
-                            finalNode = v
-                        # v ce se uzeti u razmatranje ukoliko je najbolja vrednost veca od trenutne najbolje
-                        # u duhu BnB algoritma, ako nije veca, nema potrebe razmatrati cvor v
-                    v.bound = bound(v, knapsackWeight, items)
-                    if (v.bound > maxValue):
-                        Q.put(v)
+                #if v.positionState[v.position] >=0:
+                if v.weight <= knapsackWeight and v.value > maxValue   :
+                        maxValue = v.value
+                        finalNode = v
                         
-                    while (finalNode.parent != None):
-                        if(finalNode.inserted == 1):
-                            print (items[finalNode.level])
-                        finalNode = finalNode.parent        
-            
-                    
-                    print(v.positionState)
+                #print(v.positionState)
+                    # v ce se uzeti u razmatranje ukoliko je najbolja vrednost veca od trenutne najbolje
+                    # u duhu BnB algoritma, ako nije veca, nema potrebe razmatrati cvor v
+                v.bound = bound(v, knapsackWeight, items)
+                if (v.bound > maxValue):
+                    Q.put(v)
+                            
+                #print (items[v.level])
+                
       
                 # v ovde predstavlja cvor za nivo nize pod pretpostavkom da nije uzet cvor v
-                v = Node(u.level + 1, u.weight, u.value, 0, u, items[u.level + 1].element_type, u.positionState)
+                v = Node(u.level + 1, u.weight, u.value, u, 0 , items[u.level + 1].element_type, u.positionState)
                 v.bound = bound(v, knapsackWeight, items) 
                 if (v.bound > maxValue):
                     #print('pingvin')
                     Q.put(v)
             
-
-            
+                # print (items[v.level])
+                #print(v.positionState)
+            suma = 0.0
+            while (finalNode.parent != None):
+                if(finalNode.inserted == 1):
+                    print (items[finalNode.level])
+                    suma += items[finalNode.level].now_cost/10
+                finalNode = finalNode.parent
+            print(suma)
+    
             return maxValue
                 
         
@@ -113,11 +122,12 @@ async def main():
                     
         playersSorted = sorted(players, key=lambda x: x.total_points/x.now_cost / 10 , reverse=True)
         
-        # for x in playersSorted:
+        # for x in playersSorted[0:20]:
         #     print(x)
+            
+        print('\n\n')
         
-        
-        
+        #print (decrementList([1,1,1,1],playersSorted[200].element_type))
         print('Maksimalna vrednost:', bnb(knapsackWeight, playersSorted))
 
         
@@ -126,4 +136,4 @@ async def main():
 if __name__ == "__main__":
     asyncio.create_task(main())
     
-    
+  # decrementList( u.positionState,items[u.level + 1].element_type)  
